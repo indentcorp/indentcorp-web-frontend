9a7e7dcf6b0e921b3d17ac6e026add537a60794a
1noilimrev <noilimrev@outlook.com>
2021-04-20 15:19:47 +0900

    Delete unaffiliated team members

---
+++ src/components/Modules/TeamData/TeamData.tsx
@@ -0,0 +1,127 @@
+const TeamData = {
+  data: [
+    {
+      id: '1',
+      position: 'S/W Engineer',
+      blogLink: null,
+      ghLink: null,
+      gridArea: 'auto',
+      poster:
+        'https://d2avi8not6hrpt.cloudfront.net/indentcorp.com/images/w_still.jpg',
+      videoSrc:
+        'https://d2avi8not6hrpt.cloudfront.net/indentcorp.com/videos/180/w_mov_200k.mp4',
+    },
+    {
+      id: 'Alan',
+      position: 'S/W Engineer',
+      blogLink: 'https://www.44bits.io/ko',
+      ghLink: 'https://github.com/raccoonyy/',
+      gridArea: 'auto',
+      poster:
+        'https://d2avi8not6hrpt.cloudfront.net/indentcorp.com/images/ksh_mov_200k.jpg',
+      videoSrc:
+        'https://d2avi8not6hrpt.cloudfront.net/indentcorp.com/videos/180/ksh_mov_200k.mp4',
+    },
+    {
+      id: 'Blackbull',
+      position: 'S/W Engineer',
+      blogLink: 'https://developer-park.tistory.com/',
+      ghLink: 'https://github.com/devsehyeon',
+      gridArea: 'auto',
+      poster:
+        'https://d2avi8not6hrpt.cloudfront.net/indentcorp.com/images/sh2_mod_200k.jpg',
+      videoSrc:
+        'https://d2avi8not6hrpt.cloudfront.net/indentcorp.com/videos/180/sh2_mod_200k.mp4',
+    },
+    {
+      id: 'Gyunho',
+      position: 'Operator',
+      blogLink: null,
+      ghLink: null,
+      gridArea: 'auto',
+      poster:
+        'https://d2avi8not6hrpt.cloudfront.net/indentcorp.com/images/gh_mov_200k.jpg',
+      videoSrc:
+        'https://d2avi8not6hrpt.cloudfront.net/indentcorp.com/videos/180/gh_mov_200k.mp4',
+    },
+    {
+      id: 'Hyeonho',
+      position: 'Product Manager',
+      blogLink: null,
+      ghLink: null,
+      gridArea: 'auto',
+      poster:
+        'https://d2avi8not6hrpt.cloudfront.net/indentcorp.com/images/khh_200k.jpg',
+      videoSrc:
+        'https://d2avi8not6hrpt.cloudfront.net/indentcorp.com/videos/180/khh_200k.mp4',
+    },
+    {
+      id: 'Jen',
+      position: 'CX Designer',
+      blogLink: null,
+      ghLink: null,
+      gridArea: 'auto',
+      poster:
+        'https://d2avi8not6hrpt.cloudfront.net/indentcorp.com/images/jy_mov_200k.jpg',
+      videoSrc:
+        'https://d2avi8not6hrpt.cloudfront.net/indentcorp.com/videos/180/jy_mov_200k.mp4',
+    },
+    {
+      id: 'Morgan',
+      position: 'CEO',
+      blogLink: null,
+      ghLink: null,
+      gridArea: 'auto',
+      poster:
+        'https://d2avi8not6hrpt.cloudfront.net/indentcorp.com/images/ts_still.jpg',
+      videoSrc:
+        'https://d2avi8not6hrpt.cloudfront.net/indentcorp.com/videos/180/ts_mov_200k.mp4',
+    },
+    {
+      id: 'Taetae',
+      position: 'S/W Engineer',
+      blogLink: null,
+      ghLink: 'https://github.com/taeyeon47',
+      gridArea: 'auto',
+      poster:
+        'https://d2avi8not6hrpt.cloudfront.net/indentcorp.com/images/taeyeon.jpg',
+      videoSrc:
+        'https://d2avi8not6hrpt.cloudfront.net/indentcorp.com/videos/180/taeyeon.mp4',
+    },
+    {
+      id: 'Wonhee',
+      position: 'Operator',
+      blogLink: null,
+      ghLink: null,
+      gridArea: 'auto',
+      poster:
+        'https://d2avi8not6hrpt.cloudfront.net/indentcorp.com/images/wh_mov_200k.jpg',
+      videoSrc:
+        'https://d2avi8not6hrpt.cloudfront.net/indentcorp.com/videos/180/wh.mp4',
+    },
+    {
+      id: 'Yedarm',
+      position: 'Brand Marketer',
+      blogLink: null,
+      ghLink: null,
+      gridArea: 'auto',
+      poster:
+        'https://d2avi8not6hrpt.cloudfront.net/indentcorp.com/images/yedarm.jpg',
+      videoSrc:
+        'https://d2avi8not6hrpt.cloudfront.net/indentcorp.com/videos/180/yedarm.mp4',
+    },
+    {
+      id: 'Yunsun',
+      position: 'S/W Engineer',
+      blogLink: null,
+      ghLink: 'https://github.com/yunsun1127',
+      gridArea: 'auto',
+      poster:
+        'https://d2avi8not6hrpt.cloudfront.net/indentcorp.com/images/yunsun.jpg',
+      videoSrc:
+        'https://d2avi8not6hrpt.cloudfront.net/indentcorp.com/videos/180/yunsun.mp4',
+    },
+  ],
+};
+
+export default TeamData;
