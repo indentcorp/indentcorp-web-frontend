a8a860e20d4d3110d9cb830221c5b0685ff22ef7
1noilimrev <noilimrev@outlook.com>
2021-04-23 19:38:16 +0900

    Merge MapAPI into a common module

--- src/components/Modules/Map/MapAPI.tsx
+++ src/components/Modules/Map/MapAPI.tsx
@@ -1,67 +1,13 @@
-/*global kakao*/
-import React, { useEffect } from 'react';
-import useScript from './useScript'
-declare global {
-  interface Window {
-    kakao: any
-  }
-}
+import React from "react";
 
-interface Props {
-  width: number;
-  height: number;
-}
-
-// const API_KEY = process.env.REACT_APP_KAKAO_MAP_KEY;
-
-// console.log('API_KEY', API_KEY)
-const MapAPI: React.FC = () => {
-  useScript(`https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.REACT_APP_KAKAO_MAP_KEY}&libraries=services&autoload=false`);
-  useEffect(() => {
-    // const script = document.createElement("script");
-    // script.type = "text/javascript";
-    // script.async = true;
-    // script.src =
-    //   `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.REACT_APP_KAKAO_MAP_KEY}&libraries=services&autoload=false`;
-    // // `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.REACT_APP_KAKAO_MAP_KEY}`;
-    // document.body.appendChild(script);
-    let container = document.getElementById('map');
-    let options = {
-      center: new window.kakao.maps.LatLng(37.5467145, 127.0451214),
-      level: 3,
-    }
-    let map = new window.kakao.maps.Map(container, options)
-    let geocoder = new window.kakao.maps.services.Geocoder()
-
-    // var mapTypeControl = new window.kakao.maps.MapTypeControl();
-    // map.addControl(mapTypeControl, window.kakao.maps.ControlPosition.TOPRIGHT);
-    var zoomControl = new window.kakao.maps.ZoomControl()
-    map.addControl(zoomControl, window.kakao.maps.ControlPosition.RIGHT)
-
-    geocoder.addressSearch('서울 성동구 성수동1가 656-439', function (result: any, status: any) {
-      if (status === window.kakao.maps.services.Status.OK) {
-        let coords = new window.kakao.maps.LatLng(result[0].y, result[0].x)
-
-        let marker = new window.kakao.maps.Marker({
-          map: map,
-          position: coords,
-        })
-        let iwContent =
-          '<div style="width:150px;text-align:center;padding:6px 0;"><a style="text-decoration:none;color:black;"href="https://map.kakao.com/link/to/인덴트코퍼레이션,37.5467145,127.0451214">인덴트코퍼레이션</a></div>'
-        let infowindow = new window.kakao.maps.InfoWindow({
-          content: iwContent,
-        })
-        infowindow.open(map, marker)
-        map.setCenter(coords)
-      }
-    })
-  }, [])
-
+const MapAPI = () => {
   return (
-    <div className="MapAPI">
-      <div id="map" style={{ width: '309px', height: '309px' }} />
-    </div>
-  )
-}
+    <img
+      src={`https://maps.googleapis.com/maps/api/staticmap?center=37.5406997,127.0573674&zoom=14&size=309x309&maptype=roadmap
+    &markers=color:red%7C37.5406997,127.0573674&key=${process.env.REACT_APP_GOOGLE_MAP_KEY}`}
+      alt=""
+    />
+  );
+};
 
-export default MapAPI
+export default MapAPI;
