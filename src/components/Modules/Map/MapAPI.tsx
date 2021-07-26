84f5bef2e82dbc5d7e9d369f412f2142a5b282cb
1noilimrev <noilimrev@outlook.com>
2021-07-27 05:22:15 +0900

    Fix the style value of text box in "MapAPI"

--- src/components/Modules/Map/MapAPI.tsx
+++ src/components/Modules/Map/MapAPI.tsx
@@ -21,37 +21,39 @@ const MapAPI: React.FC<Props> = ({ width, height, styles }) => {
   const latitude = 37.5407049;
   const longitude = 127.0595125;
   useEffect(() => {
-    const script = document.createElement('script');
-    script.type = "text/javascript"
-    script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.REACT_APP_KAKAO_MAP_KEY}&autoload=false`
+    const script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.REACT_APP_KAKAO_MAP_KEY}&autoload=false`;
     document.head.appendChild(script);
     script.onload = () => {
       window?.kakao.maps.load(() => {
-    let mapContainer = document.getElementById("map"),
-      mapOption = {
-        center: new window.kakao.maps.LatLng(latitude, longitude),
-        draggable: false,
-        level: 3,
-      };
-    let map = new window.kakao.maps.Map(mapContainer, mapOption);
-    let zoomControl = new window.kakao.maps.ZoomControl();
-    map.addControl(zoomControl, window.kakao.maps.ControlPosition.RIGHT);
-    let markerPosition = new window.kakao.maps.LatLng(latitude, longitude);
-    let marker = new window.kakao.maps.Marker({
-      position: markerPosition,
-    });
-    marker.setMap(map);
-    let iwContent =
-        '<div style="padding:5px; padding-left:20px;">인덴트코퍼레이션</div>',
-      iwPosition = new window.kakao.maps.LatLng(latitude, longitude);
-    let infowindow = new window.kakao.maps.InfoWindow({
-      position: iwPosition,
-      content: iwContent,
-    });
-    infowindow.open(map, marker);
-  });
-  }
-    return () => script.remove()
+        let mapContainer = document.getElementById("map"),
+          mapOption = {
+            center: new window.kakao.maps.LatLng(latitude, longitude),
+            draggable: false,
+            level: 3,
+          };
+        let map = new window.kakao.maps.Map(mapContainer, mapOption);
+        let zoomControl = new window.kakao.maps.ZoomControl();
+        map.addControl(zoomControl, window.kakao.maps.ControlPosition.RIGHT);
+        let markerPosition = new window.kakao.maps.LatLng(latitude, longitude);
+        let marker = new window.kakao.maps.Marker({
+          position: markerPosition,
+        });
+        marker.setMap(map);
+        let iwContent = `<div><a href="https://map.kakao.com/link/to/748166279"
+             style="padding:5px 5px 5px 20px; color:inherit;
+              text-decoration:none; position:relative; top:2px;">
+              인덴트코퍼레이션</a></div>`,
+          iwPosition = new window.kakao.maps.LatLng(latitude, longitude);
+        let infowindow = new window.kakao.maps.InfoWindow({
+          position: iwPosition,
+          content: iwContent,
+        });
+        infowindow.open(map, marker);
+      });
+    };
+    return () => script.remove();
   }, []);
 
   return (
