a748b2c214713e62a81fd6f00e4802193de674f6
Alan Kim <alankim@indentcorp.com>
2021-12-20 16:19:31 +0900

    google tag manager 스크립트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,13 @@ import React from 'react';
 import './App.css';
 import Main from '../src/pages/Main';
 import GlobalStyles from './components/GlobalStyles';
+import TagManager from 'react-gtm-module';
+
+const tagManagerArgs = {
+  gtmId: 'GTM-PKCPL48'
+}
+TagManager.initialize(tagManagerArgs)
+
 function App() {
   return (
     <>
